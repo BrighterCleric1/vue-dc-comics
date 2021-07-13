739640b5c9fabe2e9f0d24046a37c90b3a3ebb51
BrighterCleric1 <simontemplar99@hotmail.it>
2021-07-13 10:53:11 +0200

    Jumbo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="random"></div>
+  <div class="all">
+    <div class="jumbo">yo</div>
+    <div class="main-bg">
+      <div class="container">
+        <div class="current">CURRENT SERIES</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -11,8 +18,28 @@ export default {
 
 
 <style lang="scss" scoped>
-.random {
+@import "../style/colors.scss";
+@import "../style/fonts.scss";
+.main-bg {
+  font-family: "Open Sans", sans-serif;
   height: 200px;
   background-color: #002;
+  .container {
+    position: relative;
+    .current {
+      position: absolute;
+      color: white;
+      height: 40px;
+      top: -20px;
+      background-color: $blue;
+      padding: 10px;
+    }
+  }
+}
+.jumbo {
+  height: 250px;
+  background-image: url("../assets/jumbotron.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
